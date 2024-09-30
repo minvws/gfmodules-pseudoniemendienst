8ba9a76192ff896cd9e970625e93906c2750923d
Joshua Thijssen <jthijssen@noxlogic.nl>
2024-09-30 09:43:32 +0200

    Added TLS certificate tests

--- app/services/tls_service.py
+++ app/services/tls_service.py
@@ -1,6 +1,8 @@
 from cryptography import x509
-from cryptography.hazmat.primitives.asymmetric import ec, rsa
+from cryptography.hazmat.primitives.asymmetric import ec, rsa, ed25519, ed448
+from cryptography.x509 import Certificate
 from cryptography.x509.oid import ExtensionOID
+import validators
 
 
 class CertAuthentication(str):
@@ -14,6 +16,9 @@ class CertAuthentications:
 
 
 class CertAuthenticationException(Exception):
+    pass
+
+class CertValidationException(Exception):
     pass
 
 class TLSService:
@@ -38,42 +43,72 @@ class TLSService:
         """
         try:
             cert = x509.load_pem_x509_certificate(pem)
+            return self.validate_cert_elements(cert)
         except ValueError:
             return False
 
+    def validate_cert_elements(self, cert: Certificate) -> bool:
         if isinstance(cert.public_key(), ec.EllipticCurvePublicKey):
             curve_name = cert.public_key().curve.name       # type: ignore
             if curve_name not in self.curves:
-                return False
-
-        if isinstance(cert.public_key(), rsa.RSAPublicKey):
+                raise CertValidationException(f"Curve {curve_name} is not allowed")
+        elif isinstance(cert.public_key(), ed25519.Ed25519PublicKey):
+            pass
+        elif isinstance(cert.public_key(), ed448.Ed448PublicKey):
+            pass
+        elif isinstance(cert.public_key(), rsa.RSAPublicKey):
             if cert.public_key().key_size < self.min_bit_size:      # type: ignore
-                return False
+                raise CertValidationException(f"RSA key size is too small: {cert.public_key().key_size}") # type: ignore
+        else:
+            raise CertValidationException("Invalid key type")
 
         # No wildcards in CN are allowed
         subject = cert.subject
         cn = subject.get_attributes_for_oid(x509.NameOID.COMMON_NAME)
-        if cn is None or len(cn) < 1 or "*" in cn[0].value:
-            return False
+        if len(cn) > 1:
+            # probably not possible
+            raise CertValidationException("Multiple CNs are not allowed")
+        if len(cn) == 1:
+            if "*" in cn[0].value:
+                raise CertValidationException("Wildcard in CN is not allowed")
+            if not validators.domain(cn[0].value):
+                raise CertValidationException("CN is not a valid domain")
 
-        # Check that the SAN extension is present and that first element matches the CN
         try:
             san_extension = cert.extensions.get_extension_for_oid(ExtensionOID.SUBJECT_ALTERNATIVE_NAME)
-            if cn[0].value not in san_extension.value.get_values_for_type(x509.DNSName):    # type: ignore
-                return False
+
+            # Check that the CN is inside the SAN as well
+            if len(cn) == 1:
+                dns_entries = san_extension.value.get_values_for_type(x509.DNSName) # type: ignore
+                if cn[0].value not in dns_entries:
+                    raise CertValidationException("CN is not in SAN")
+                print(cn[0].value)
+                print(validators.domain(cn[0].value))
+                if not validators.domain(cn[0].value):
+                    raise CertValidationException("SAN is not a valid domain")
+
+            # If no CN is given, check if the FIRST san entry is a valid domain
+            if len(cn) == 0:
+                # if we didn't have a common name, we ONLY check the first SAN entry
+                first_entry = san_extension.value[0]        # type: ignore
+                if not isinstance(first_entry, x509.DNSName):
+                    raise CertValidationException("First SAN entry is not a DNSName")
+
+                if not validators.domain(first_entry.value):
+                    raise CertValidationException("First SAN entry is not a valid domain")
         except x509.ExtensionNotFound:
             # MUST have a SAN extension
-            return False
+            raise CertValidationException("No SAN extension found")
 
         # Check key usage
         try:
             extended_key_usage = cert.extensions.get_extension_for_oid(ExtensionOID.EXTENDED_KEY_USAGE)
             if x509.oid.ExtendedKeyUsageOID.SERVER_AUTH not in extended_key_usage.value:    # type: ignore
-                return False
+                raise CertValidationException("No server authentication found in key usage")
             if x509.oid.ExtendedKeyUsageOID.CLIENT_AUTH not in extended_key_usage.value:    # type: ignore
-                return False
+                raise CertValidationException("No client authentication found in key usage")
         except x509.ExtensionNotFound:
-            return False
+            raise CertValidationException("No extended key usage found")
 
         return True
 
