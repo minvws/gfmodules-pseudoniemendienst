6e4b5506ab37d5c96433e003ac7124ef1ad60140
MelchiorKokernoot <melchiorkokernoot@gmail.com>
2024-10-14 11:38:21 +0200

    feat: change prs docker setup to work in docker compose

--- docker/init.sh
+++ docker/init.sh
@@ -17,5 +17,12 @@ else
     cp app.conf.autopilot app.conf
 fi
 
+echo "➡️ Copying the auth_cert.json file"
+if [ -e auth_cert.json ]; then
+    echo "⚠️ auth_cert_json file already exists. Skipping."
+else
+    cp auth_cert.json.example auth_cert.json
+fi
+
 echo "Start main process"
 python -m app.main
--- tools/download_certs.sh
+++ tools/download_certs.sh
@@ -6,20 +6,26 @@ SECRETS_DIR=secrets
 
 echo "Downloading generated certs from secrets container"
 
-response=$(curl --write-out '%{http_code}' --output /dev/null http://secrets/README.md)
+for i in {1..3}; do
+  response=$(curl --write-out '%{http_code}' --output /dev/null http://secrets/certs/prs/prs.crt)
 
-if [[ "$response" -ne 200 ]] ; then
-  echo "Unable to download secrets from secrets container"
-  echo "Did you start the docker compose project from the coordination repo?"
-  exit 1
-fi
+  if [[ "$response" -eq 200 ]] ; then
+    break
+  fi
 
-curl -f http://secrets/prs/prs.crt -o $SECRETS_DIR/prs.crt
-curl -f http://secrets/prs/prs.key -o $SECRETS_DIR/prs.key
-curl -f http://secrets/prs-client-1/prs-client-1.crt -o $SECRETS_DIR/prs-client-1.crt
-curl -f http://secrets/prs-client-1/prs-client-1.key -o $SECRETS_DIR/prs-client-1.key
-curl -f http://secrets/prs-client-2/prs-client-2.crt -o $SECRETS_DIR/prs-client-2.crt
-curl -f http://secrets/prs-client-2/prs-client-2.key -o $SECRETS_DIR/prs-client-2.key
-curl -f http://secrets/uzi-server-ca.crt -o $SECRETS_DIR/uzi-server-ca.crt
+  echo "Attempt $i: Unable to download secrets from secrets container"
+
+  if [[ "$i" -eq 3 ]]; then
+    exit 1
+  fi
+
+  sleep 5
+done
+
+curl -f http://secrets/certs/prs/prs.crt -o $SECRETS_DIR/prs.crt
+curl -f http://secrets/certs/prs/prs.key -o $SECRETS_DIR/prs.key
+curl -f http://secrets/certs/prs-client-1/prs-client-1.crt -o $SECRETS_DIR/prs-client-1.crt
+curl -f http://secrets/certs/prs-client-1/prs-client-1.key -o $SECRETS_DIR/prs-client-1.key
+curl -f http://secrets/certs/uzi-server-ca.crt -o $SECRETS_DIR/uzi-server-ca.crt
 
 echo "Downloaded generated certs from secrets container"
