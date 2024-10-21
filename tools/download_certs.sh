24529fee001227bd75741997a2077f99def70459
MelchiorKokernoot <melchiorkokernoot@gmail.com>
2024-10-21 10:41:04 +0200

    feature: extend certs downloader script to accept argument

--- tools/download_certs.sh
+++ tools/download_certs.sh
@@ -4,28 +4,40 @@ set -e
 
 SECRETS_DIR=secrets
 
+mkdir -p $SECRETS_DIR
+
+download() {
+  local cert_name=$1
+  local crt_url="http://secrets/certs/$cert_name/$cert_name.crt"
+  local key_url="http://secrets/certs/$cert_name/$cert_name.key"
+
+  for i in {1..3}; do
+    response=$(curl --write-out '%{http_code}' --output /dev/null $crt_url)
+
+    if [[ "$response" -eq 200 ]]; then
+      break
+    fi
+
+    echo "Attempt $i: Unable to download $cert_name cert from secrets container"
+
+    if [[ "$i" -eq 3 ]]; then
+      exit 1
+    fi
+
+    sleep 5
+  done
+
+  echo "Downloading certificate: $cert_name"
+  curl -f $crt_url -o $SECRETS_DIR/$cert_name.crt
+  curl -f $key_url -o $SECRETS_DIR/$cert_name.key
+}
+
 echo "Downloading generated certs from secrets container"
 
-for i in {1..3}; do
-  response=$(curl --write-out '%{http_code}' --output /dev/null http://secrets/certs/prs/prs.crt)
-
-  if [[ "$response" -eq 200 ]] ; then
-    break
-  fi
-
-  echo "Attempt $i: Unable to download secrets from secrets container"
-
-  if [[ "$i" -eq 3 ]]; then
-    exit 1
-  fi
-
-  sleep 5
+for cert_name in "$@"; do
+  download "$cert_name"
 done
 
-curl -f http://secrets/certs/prs/prs.crt -o $SECRETS_DIR/prs.crt
-curl -f http://secrets/certs/prs/prs.key -o $SECRETS_DIR/prs.key
-curl -f http://secrets/certs/prs-client-1/prs-client-1.crt -o $SECRETS_DIR/prs-client-1.crt
-curl -f http://secrets/certs/prs-client-1/prs-client-1.key -o $SECRETS_DIR/prs-client-1.key
 curl -f http://secrets/certs/uzi-server-ca.crt -o $SECRETS_DIR/uzi-server-ca.crt
 
 echo "Downloaded generated certs from secrets container"
