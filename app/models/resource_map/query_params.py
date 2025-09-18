241f179f63b21ddd228a6aac9582a2950ff8c308
Joshua Thijssen <jthijssen@noxlogic.nl>
2025-09-18 10:14:22 +0200

    removed obsolete files

--- app/models/resource_map/query_params.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-
-class ResourceMapQueryParams(BaseModel):
-    directory_id: str | None = None
-    resource_type: str | None = None
-    directory_resource_id: str | None = None
-    update_client_resource_id: str | None = None
