941dfd4656180a541b084c25b71cf272b055918e
Rick Lambrechts <rick@rl-webdiensten.nl>
2026-05-19 11:19:30 +0200

    Use log warnings instead of errors when client 4xx errors

--- app/routers/oprf.py
+++ app/routers/oprf.py
@@ -26,7 +26,7 @@ def post_eval(
 ) -> JSONResponse:
 
     if not req.recipientOrganization.startswith("ura:"):
-        logger.error("does not start with ura: %s" % req.recipientOrganization)
+        logger.warning("does not start with ura: %s" % req.recipientOrganization)
         return JSONResponse(
             {"error": "Invalid recipient organization. Format: ura:<ura_number>"},
             status_code=400,
@@ -35,14 +35,14 @@ def post_eval(
 
     org = org_service.get_by_ura(ura)
     if org is None:
-        logger.error("no organization found for URA %r", ura)
+        logger.warning("no organization found for URA %r", ura)
         return JSONResponse(
             {"error": "No organization found for this ura"}, status_code=404
         )
 
     pub_key_jwk = key_resolver.resolve(org.id, req.recipientScope)
     if pub_key_jwk is None:
-        logger.error(
+        logger.warning(
             "no public key found for organization %r and scope %r",
             org.id,
             req.recipientScope,
@@ -55,7 +55,7 @@ def post_eval(
     try:
         jwe_str = oprf_service.eval_blind(req, pub_key_jwk)
     except ValueError as e:
-        logger.exception("unable to evaluate blind: %r", e)
+        logger.warning("unable to evaluate blind: %r", e)
         return JSONResponse({"error": "Unable to evaluate blind"}, status_code=400)
 
     return JSONResponse({"jwe": jwe_str})
